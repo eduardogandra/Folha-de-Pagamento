e33f3731a767db5b1320fa17952f564249c9a259
Beatriz Ribas <beatriz.ribas09@hotmail.com>
2017-04-18 01:40:01 -0300

    Create CalculoFolha.cs

---
+++ FolhaDePagamento/Controller/CalculoFolha.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FolhaDePagamento.DAL;
+using FolhaDePagamento.Model;
+
+namespace FolhaDePagamento.Controller
+{
+    class CalculoFolha
+    {
+        public double SalarioBruto (int horas, double valorHoras)
+        {
+            return 0;
+        }
+
+        public double SalarioLiquido (int horas, double valorHoras)
+        {
+            return 0;
+        }
+
+        public double CalculoINSS(int horas, double valorHoras)
+        {
+            return 0;
+        }
+
+        public double CalculoFGTS(int horas, double valorHoras)
+        {
+            return 0;
+        }
+
+        public double CalculoIR(int horas, double valorHoras)
+        {
+            return 0;
+        }
+    }
+}
