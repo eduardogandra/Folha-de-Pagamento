1420e1b394a2ebe440e41627986a588c58f63662
Beatriz Ribas <beatriz.ribas09@hotmail.com>
2017-04-17 21:48:38 -0300

    Update Program.cs

--- FolhaDePagamento/Program.cs
+++ FolhaDePagamento/Program.cs
@@ -12,36 +12,44 @@ namespace FolhaDePagamento
         {
             Funcionario funcionario = new Funcionario();
             Folha folha = new Folha();
-            int opt = 0;
-             
-            Console.WriteLine("Olá, vamos ao que interessa. Dinheiro.");
-            Console.WriteLine("Diga agora o que deseja fazer!");
-            Console.WriteLine("Escolha abaixo as opções disponíveis: ");
-            opt = '9';
+            string opcao;
+            
             do
             {
-                Console.WriteLine("1 -- Cadastrar funcionário");
-                Console.WriteLine("2 -- Cadastrar folha de pagamento");
-                Console.WriteLine("3 -- Listar Folhas de Pagamento");
-                Console.WriteLine("0 -- SAIR");
-            } while (opt != '0');
-             switch(opt){
-                 case 1: 
-                     Console.Clear();
-                     Console.WriteLine ("--** - Cadastrar Funcionários - **--");
-                     break;
-                 case 2:
-                     Console.Clear();
-                     Console.WriteLine("--** - Cadastrar Folha de Pagamento - **--");
-                     break;
-                 case 3: 
-                     Console.Clear();
-                     Console.WriteLine("--** - Listar Folhas de Pagamento - **--");
-                     break;
-                     case '0':
-                         Console.Clear();
+                Console.Clear();
+                Console.WriteLine(" -- Sistema de Folha de Pagamento -- ");
+                Console.WriteLine("\n1 - Cadastrar Funcionário");
+                Console.WriteLine("2 - Cadastrar Folha de pagamento");
+                Console.WriteLine("3 - Listar Folhas de Pagamento");
+                Console.WriteLine("0 - SAIR");
+                opcao = Console.ReadLine();
 
-             }
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("--** - Cadastrar Funcionários - **--");
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("--** - Cadastrar Folha de Pagamento - **--");
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("--** - Listar Folhas de Pagamento - **--");
+                        break;
+                    case "0":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção Inválida!");
+                        break;
+                }
+                Console.WriteLine("Aperte uma tecla para continuar...");
+                Console.ReadKey();
+            } while (!opcao.Equals ("0"));  
+             
         }
     }
 }
