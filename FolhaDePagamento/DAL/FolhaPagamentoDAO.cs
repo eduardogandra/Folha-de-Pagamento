31481ba5876264b5b7687a0cd61c1d397b602533
Beatriz Ribas <beatriz.ribas09@hotmail.com>
2017-04-18 16:10:56 -0300

    Update FolhaPagamentoDAO.cs

--- FolhaDePagamento/DAL/FolhaPagamentoDAO.cs
+++ FolhaDePagamento/DAL/FolhaPagamentoDAO.cs
@@ -29,5 +29,33 @@ namespace FolhaDePagamento.DAL
             }
             return folhaPagamentoAux;
         }
+        
+         public static List<FolhaPagamento> BuscarFolhaPagamentoMes(FolhaPagamento folhasPagamentos)
+        {
+            List<FolhaPagamento> folhaPagamentoAux = new List<FolhaPagamento>();
+            foreach (FolhaPagamento folhaPagamentoCadastrada in folhaPagamento)
+            {
+                if (folhaPagamentoCadastrada.Mes.Equals(folhasPagamentos.Mes))
+                {
+                    folhaPagamentoAux.Add(folhaPagamentoCadastrada);
+                    
+                }
+            }
+            return folhaPagamentoAux;
+        }
+
+        public static List<FolhaPagamento> BuscarFolhaPagamentoAno(FolhaPagamento folhasPagamentos)
+        {
+            List<FolhaPagamento> folhaPagamentoAux = new List<FolhaPagamento>();
+            foreach (FolhaPagamento folhaPagamentoCadastrada in folhaPagamento)
+            {
+                if (folhaPagamentoCadastrada.Ano.Equals(folhasPagamentos.Ano))
+                {
+                    folhaPagamentoAux.Add(folhaPagamentoCadastrada);
+
+                }
+            }
+            return folhaPagamentoAux;
+        }
     }
 }
